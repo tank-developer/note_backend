35ca7fb20fba9c93fe56df4dbdbe4a26b0f82d20
“坦克” <1051136697@qq.com>
2025-02-10 22:53:56 +0800

    commit6

--- note/src/main/java/org/company/note/utils/alyoss/AliOssUtil.java
+++ note/src/main/java/org/company/note/utils/alyoss/AliOssUtil.java
@@ -17,7 +17,7 @@ import java.io.ByteArrayInputStream;
 public class AliOssUtil {
 
     private String endpoint = "xxx";
-    private String accKey = "xxx";
+    private String accKe = "xxx";
     private String secret = "xxx";
     private String bucketName = "note-content";
 
@@ -31,7 +31,7 @@ public class AliOssUtil {
     public String upload(byte[] bytes, String objectName) {
 
         // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accKey, secret);
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accKe, secret);
 
         try {
             // 创建PutObject请求。
@@ -74,7 +74,7 @@ public class AliOssUtil {
      * @return
      */
     public String delete(String objectName) {
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accKey, secret);
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accKe, secret);
         try {
             // 删除文件。
             ossClient.deleteObject(bucketName, objectName);
